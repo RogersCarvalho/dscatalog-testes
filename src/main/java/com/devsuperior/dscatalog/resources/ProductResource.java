f428b50dea2d9709fa4272c27836f5595124b3d6
RogersCarvalho <rogerssud@yahoo.com.br>
2022-03-09 13:11:28 -0300

    refatorar usando o pageable

--- src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
+++ src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,32 +18,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import com.devsuperior.dscatalog.dto.ProductDTO;
+
+
 import com.devsuperior.dscatalog.services.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductResource {
 
+	
 	@Autowired
 	private ProductService service;
 	
 	@GetMapping
-	public ResponseEntity<Page<ProductDTO>> findAll(
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-			) {
+	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
 		
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		//A variável pageable é instanciada a partir da classe Pageable
+		//A variável pageable armazena um objeto que possui os atributos page, size,sort
+		//O page substitui o page, o size substitui o linesPerPage, o 
+		//sort substitui Direction.valueOf(direction) e o  orderBy
+		//assim é preciso apenas informar pageable
+		
+		//PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 				
-		Page<ProductDTO> list = service.findAllPaged(pageRequest);
+		Page<ProductDTO> list = service.findAllPaged(pageable);
 		
 		return ResponseEntity.ok().body(list);
-	}
+	    }
+	
 
+	
+	
+	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 		ProductDTO dto = service.findById(id);
